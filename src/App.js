4b2e216613de3ae1a96ee246cae7179c744aaf82
Thomas Matysik <thomas@matysik.co.nz>
2017-08-19 18:15:15 +1200

    Remove the demo crap

--- src/App.js
+++ src/App.js
@@ -1,103 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 
-//components
-import Emoji from './Emoji';
+// components
 
-//styles
-import './App.scss';
-import './App.less';
-import './App.styl';
-
-//modules
-import cssStyles from './First.module.css';
-import sassStyles from './Second.module.scss';
-import lessStyles from './Third.module.less';
-import stylusStyles from './Fourth.module.styl';
+// styles
+import cssStyles from './App.module.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className={cssStyles.header}>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2 className="App-title">
-            <Emoji label="danger" emoji="☢" />
-            <span> custom-react-scripts </span>
-            <Emoji label="danger" emoji="☢" />
+      <div className={ cssStyles.app }>
+        <div className={ cssStyles.header }>
+          <img src={ logo } className={ cssStyles.logo } alt='logo' />
+          <h2 className={ cssStyles.title }>
+            Electronic DC Load
           </h2>
-          <div className="App-subtitle">
+          <div className={ cssStyles.subtitle }>
             allow custom config for create-react-app without ejecting
           </div>
         </div>
 
-        <div className={stylusStyles.description}>
-          <div className={sassStyles.command}>
-            <code>
-              create-react-app my-app --scripts-version custom-react-scripts
-            </code>
-          </div>
-
+        <div className={ cssStyles.description }>
           <p>
-            If you want to enable/disable certain features just modify the
-            <b> .env</b> file in the root directory of the project.
+            Content goes here.
           </p>
-
-          <b> Styling </b>
-          <ul className="configs style-configs">
-            <li>
-              <code>REACT_APP_SASS=true</code>
-              <span>- Enable SASS</span>
-            </li>
-            <li>
-              <code>REACT_APP_LESS=true</code>
-              <span>- Enable LESS</span>
-            </li>
-            <li>
-              <code>REACT_APP_STYLUS=true</code>
-              <span>- Enable Stylus</span>
-            </li>
-            <li>
-              <code>REACT_APP_CSS_MODULES=true</code>
-              <span>- Enable CSS modules </span>
-            </li>
-            <li>
-              <code>REACT_APP_SASS_MODULES=true</code>
-              <span>- Enable Sass modules </span>
-            </li>
-            <li>
-              <code>REACT_APP_SASS_MODULES=true</code>
-              <span>- Enable Stylus modules </span>
-            </li>
-            <li>
-              <code>REACT_APP_SASS_MODULES=true</code>
-              <span>- Enable Less modules </span>
-            </li>
-          </ul>
-
-          <b>Babel</b>
-
-          <ul className="configs babel-configs">
-            <li>
-              <code>REACT_APP_BABEL_STAGE_0=true</code>
-              <span>- Enable stage-0 preset</span>
-            </li>
-            <li>
-              <code>REACT_APP_DECORATORS=true</code>
-              <span>- Enable usage of decorators</span>
-            </li>
-          </ul>
-
-          <br />
-          <br />
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className={lessStyles.readmeLink}
-            href="https://github.com/kitze/create-react-app/tree/master/packages/react-scripts"
-          >
-            Link to full README.md
-          </a>
         </div>
       </div>
     );
